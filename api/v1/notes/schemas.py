b495eabb1bdcbcd7e7b305b9f6f87f92000b165c
Dima Saveliev <dp236955@gmail.com>
2025-03-15 21:46:15 +0200

    Create notes schemas
    schemas created:
        - CreateNoteSchema
        - UpdateNoteSchema
        - NoteSchema
        - NoteHistorySchema
        - NoteSchemaWithHistory

---
+++ api/v1/notes/schemas.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
+
+class CreateNoteSchema(BaseModel):
+    title: str = Field(min_length=3, max_length=150)
+    text: str
+
+
+class UpdateNoteSchema(BaseModel):
+    title: str | None = Field(min_length=3, max_length=150, default=None)
+    text: str | None = None
+
+
+class NoteSchema(CreateNoteSchema):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+    user_id: int
+
+
+class NoteHistorySchema(CreateNoteSchema):
+    id: int
+    created_at: datetime
+    note_id: int
+
+
+class NoteSchemaWithHistory(CreateNoteSchema):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+    user_id: int
+    note_history: list[NoteHistorySchema]
